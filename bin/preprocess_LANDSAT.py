0d7fbd5ed0756db0682a3721e52ec5505b179561
Sam Murphy <samsammurphy@gmail.com>
2017-01-10 13:34:48 -1000

    added Satellite class to clarify LANDSAT preprocessing

--- bin/preprocess_LANDSAT.py
+++ bin/preprocess_LANDSAT.py
@@ -9,94 +9,77 @@ renames bands, convert from DN to radiance/TOA)
 import ee
 
 
-# satellite ID
-def satellite_ID(img):
+class Satellite():
+  """
+  Tests to see which satellite mission(s) this image belongs to
+  """
+    
+  def ID(img):
+    """
+    Landsat satellite ID (i.e. first three letters of system:index)
+    """
+    return ee.String(img.get('system:index')).slice(0,3)
+  
+  def isL4or5(satID):
+    """
+    Is image from Landsat 4 or 5? (i.e. Thematic Mapper)
+    """
+    isL4 = satID.rindex('LT4').eq(0)
+    isL5 = satID.rindex('LT5').eq(0)
+    return isL4.Or(isL5)
+    
+  def isL7(satID):
+    """
+    Is image from Landsat 7? (i.e. Enhanced Thematic Mapper Plus)
+    """
+    return satID.rindex('LE7').eq(0)
+    
+  def isL8(satID):
+    """
+    Is image from Landsat 8? (i.e. Ocean-Land Imager and Thermal Imager)
+    """
+    return satID.rindex('LC8').eq(0)
+    
+    
+# vnir spectral subset
+def vnirNums(satID):
+  return ee.Algorithms.If(Satellite.isL8(satID),['B2','B3','B4','B5'],['B1','B2','B3','B4'])
+  
+# swir spectral subset
+def swirNums(satID):
+  return ee.Algorithms.If(Satellite.isL8(satID),['B6','B7'],['B5','B7'])
 
-  return ee.String(img.get('system:index')).slice(0,3)
-  
-  
-# visible-to-shortwave band names
-def get_vswirNames(satID):
-  
-  vswirNames_dict = ee.Dictionary({
-  'LT4':['blue','green','red','nir','swir1','swir2'],\
-  'LT5':['blue','green','red','nir','swir1','swir2'],\
-  'LE7':['blue','green','red','nir','swir1','swir2'],\
-  'LC8':['blue','green','red','nir','swir1','swir2'],\
-  })
-  
-  return vswirNames_dict.get(satID)
-
-# visible-to-shortwave band numbers
-def get_vswirNums(satID):
-  
-  vswirNums_dict = ee.Dictionary({
-  'LT4':['B1','B2','B3','B4','B5','B7'],\
-  'LT5':['B1','B2','B3','B4','B5','B7'],\
-  'LE7':['B1','B2','B3','B4','B5','B7'],\
-  'LC8':['B2','B3','B4','B5','B6','B7'],\
-  })
-  
-  return vswirNums_dict.get(satID)
-
-# thermal infrared band names
-def get_tirNames(satID):
-  
-  tirNames_dict = ee.Dictionary({
-  'LT4':['tir1'],\
-  'LT5':['tir1'],\
-  'LE7':['tir1','tir2'],\
-  'LC8':['tir1','tir2'],\
-  })
-  
-  return tirNames_dict.get(satID)
-  
-# thermal band numbers
-def get_tirNums(satID):
-  
-  tirNums_dict = ee.Dictionary({
+# tir spectral subset
+def tirNums(satID):
+    
+  dic = ee.Dictionary({
   'LT4':['B6'],\
   'LT5':['B6'],\
   'LE7':['B6_VCID_1','B6_VCID_2'],\
   'LC8':['B10','B11'],\
   })
   
-  return tirNums_dict.get(satID)
-  
-# vswir spectral subset
-def vswir_subset(img, satID):
-  vswirNums = ee.List(get_vswirNums(satID))
-  vswirNames = ee.List(get_vswirNames(satID))
-  subset = img.select(vswirNums,vswirNames)\
-    .set('satID',satID)\
-    .set('vswirNums',vswirNums)\
-    .set('vswirNames',vswirNames)
-  return subset
+  return dic.get(satID)
 
-# tir spectral subset
-def tir_subset(img, satID):
-  tirNums = get_tirNums(satID)
-  tirNames = get_tirNames(satID)
-  subset = img.select(tirNums,tirNames)\
-    .set('satID',satID)\
-    .set('tirNums',tirNums)\
-    .set('tirNames',tirNames)
+def tirNames(satID):
+  return ee.Algorithms.If(Satellite.isL4or5(satID),['tir1'],['tir1','tir2'])
   
-  return subset
-
 #at-sensor radiance
 def toRad(img):
-  satID = satellite_ID(img)
   rad = ee.Algorithms.Landsat.calibratedRadiance(img)
-  vswir = vswir_subset(rad, satID)
-  tir = tir_subset(rad,satID)
-  return vswir.addBands(tir)
+  satID = Satellite.ID(img)
+  vnir = rad.select(vnirNums(satID),['blue','green','red','nir'])
+  swir = rad.select(swirNums(satID),['swir1','swir2'])
+  tir = rad.select(tirNums(satID),tirNames(satID))
+
+  return ee.Dictionary({'vnir':vnir,'swir':swir,'tir':tir})
 
 #top-of-atmosphere reflectance
 def toToa(img):
-  satID = satellite_ID(img)
   toa = ee.Algorithms.Landsat.TOA(img)
-  vswir = vswir_subset(toa, satID) 
-  tir = tir_subset(toa,satID)
-  return vswir.addBands(tir)
+  satID = Satellite.ID(img)
+  vnir = toa.select(vnirNums(satID),['blue','green','red','nir'])
+  swir = toa.select(swirNums(satID),['swir1','swir2'])
+  tir = toa.select(tirNums(satID),tirNames(satID))
 
+  return ee.Dictionary({'vnir':vnir,'swir':swir,'tir':tir})
