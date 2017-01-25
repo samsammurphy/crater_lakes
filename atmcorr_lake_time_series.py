1f56e0ab298cc4070ef4da7ccfb9868a8a04faa3
Sam Murphy <samsammurphy@gmail.com>
2017-01-24 16:40:15 -1000

    significant overhaul: GEE data loaded from GeoJSON, load_iLUTs() is internal, estimate_lake_AOT() added internally, code cleaner and easier to read

--- atmcorr_lake_time_series.py
+++ atmcorr_lake_time_series.py
@@ -18,9 +18,10 @@ import glob
 import json
 import pickle
 
-#from target_altitude import target_altitude
-#from get_AOT import get_AOT
-#from surface_reflectance import surface_reflectance
+import numpy
+
+from target_altitude import target_altitude
+from surface_reflectance import surface_reflectance
 
 
 
@@ -56,8 +57,56 @@ def load_iLUTs(satellite,aerosol):
   return iLUTs
 
 
+import numpy as np
+from surface_reflectance import surface_reflectance
+
+
+
+def estimate_lake_AOT(vnir,swir,params, iLUTs):
+  """
+  Estimates aerosol optical thickness over crater lake using the water pixel 
+  ratio method (Kaufmann et al. 2008)
   
-def atmospheric_correction_time_series(target, satellite, iLUTs):
+  swir / nir = 0.81
+  
+  where,  
+  nir = near infrared radiance at 0.8 microns
+  swir = short-wave infrared radiance 2.2 microns
+  """
+  
+  # define 2.2 micron swir band
+  if params['satellite'] == 'AST':
+    swir_band = 'swir3'
+  else:
+    swir_band = 'swir2'
+    
+  # define model values for AOT
+  model_values = [0.001]
+  while np.max(model_values)*1.05 < 3:
+    model_values.append(np.max(model_values)*1.05) 
+  
+  # calculate (potential) surface reflectance ratios for each model AIT value
+  ratios = []
+  for AOT in model_values:
+    
+    params['AOT'] = AOT # set model value to params dic 
+
+    srNir = surface_reflectance(vnir['nir'], iLUTs['nir'],params)
+    
+    srSwir = surface_reflectance(swir[swir_band], iLUTs[swir_band],params)
+    
+    ratios.append(srSwir / srNir)
+       
+  # Find best result (i.e. where ratio closest to 0.81)
+  delta = abs(np.array(ratios)-0.81)
+  index = np.where(delta == np.min(delta))[0][0]
+  match = model_values[index]
+    
+  return match
+
+  
+  
+def atmospherically_correct_time_series(target, satellite, aerosol):
   """
   Apply atmospheric correction over a time series for a given target and satellite 
   """
@@ -67,81 +116,107 @@ def atmospheric_correction_time_series(target, satellite, iLUTs):
   with open(base_path+'{0}/{1}_{0}.geojson'.format(target,satellite)) as f: 
     GEE_data = json.load(f)
   
-  features = GEE_data['features']
+  iLUTs = load_iLUTs(satellite,aerosol)
   
-  # iterate over feature
-  feature = features[0]
+  altitude = target_altitude(target)
   
-  fileID = feature['id']
-  properties = feature['properties']
- 
-  print(fileID)
-  print(properties.keys())
+  results = []
+  
+  for feature in GEE_data['features']:
+     
+    properties = feature['properties']
+    
+    vnir = properties['vnir']['mean_radiance']
+    swir = properties['swir']
+        
+    # validity testing
+    valid = True
+    if properties['vnir']['pixel_counts']['lake'] == 0: # lake pixels found?
+      valid = False
+    if properties['solar_z'] > 60: 
+      valid = False
+    if not swir['swir1']: # i.e. both ASTER and LANDSAT SWIR can be OFF
+      valid = False
+    # TODO catch no data (?)
+    
+    if valid:
+      
+      print(properties)
+         
+      params = {
+                'solar_z':properties['solar_z'],
+                'H2O':properties['H2O'],
+                'O3':properties['O3'],
+                'alt':altitude,
+                'doy':properties['doy'],
+                'satellite':satellite
+                }
+      
+
              
+      params['AOT'] = estimate_lake_AOT(vnir,swir,params,iLUTs)
+         
+      sr = {} # surface reflectance
   
-#  output = []
-#  i=0
-#  if lake_data:
-#  
-#    for data in lake_data:
-#      
-#      print(i,data['fileID'])
-#      i += 1
-#                                                   # MUST..
-#      if data['lake_count'] == 0:                  # 1) detect water pixels
-#        print('No lake water pixels found')        #
-#                                                   #
-#      elif data['solar_z'] > 60:                   # 2) have solar zenith < 60
-#        print('Solar zenith is > 60')              #
-#                                                   #
-#      elif all_vswir_exists(data, vswirNames) == 0:# 3) have all vswir channels
-#        print('Not all VSWIR channels available')  #
-#                                                   #
-#      else:                                        # ..to be valid..
-#      
-#        # add altitude to data
-#        data['alt'] = target_altitude(target)
-#          
-#        # AOT estimate
-#        data['AOT'] = get_AOT(data, sensor_iLUTs)
-#        
-#        # surface reflectance
-#        refs = {}
-#        for vswirName in vswirNames:
-#          radiance = data['lake_rad'][vswirName]
-#          iLUT = sensor_iLUTs[vswirName]
-#          refs[vswirName] = surface_reflectance(radiance, iLUT, data)
-#        
-#        data['refs'] = refs
-#        output.append(data)
-#
-#
-#  # sort chronologically 
-#  def datesort(dictionary): return dictionary['date'].timestamp()
-#  output = sorted(output,key=datesort)
-#  
-#  # save file
-#  indir = "/home/sam/git/crater_lakes/atmcorr/time_series/"+target
-#  try:
-#    os.chdir(indir)
-#  except:
-#    os.mkdir(indir)
-#    os.chdir(indir)
-#  pickle.dump(output,open("{}_{}.p".format(target,satellite),"wb"))
-#
+      # VNIR    
+      for band in ['blue','green','red','nir']:
+        try:
+          radiance = vnir[band]
+          sr[band] = surface_reflectance(radiance, iLUTs[band], params)
+        except:
+          pass
+    
+      # SWIR
+      for band in ['swir1','swir2','swir3','swir4','swir5','swir6']:
+        try:
+          radiance = swir[band]
+          sr[band] = surface_reflectance(radiance, iLUTs[band], params)
+        except:
+          pass
+      
+      # Timestamp
+      unix_time = properties['date']['value'] / 1000 # i.e. GEE uses milliseconds
+        
+      result = {
+                'fileID': feature['id'],
+                'timestamp': unix_time, 
+                'sr':sr
+                }
+        
+      results.append(result)    
+
+  # chronological sort 
+  def chronological(dictionary): return dictionary['timestamp']
+  results = sorted(results,key=chronological)
+  
+  return results
+  
+def pickle_result(result, target, satellite):
+  
+  outdir = "/home/sam/git/crater_lakes/atmcorr/results/"+target
+  try:
+    os.chdir(outdir)
+  except:
+    os.mkdir(outdir)
+    os.chdir(outdir)
+    
+  pickle.dump(result,open("{}_{}.p".format(target,satellite),"wb"))
+
 
   
 def main():
   
   target = 'Aoba'
   
-  satellite = 'L4'
-
-  aerosol = 'MA'# CO needs interpolation for ASTER and other LANDSATs
+  satellite = 'L7'
   
-  iLUTs = load_iLUTs(satellite,aerosol)
-   
-  atmospheric_correction_time_series(target, satellite, iLUTs)
+  aerosol = 'MA'# TODO CO needs interpolation for ASTER and other LANDSATs
+     
+  result = atmospherically_correct_time_series(target, satellite, aerosol)
+  
+  pickle_result(result, target, satellite)
+  
+  print('done')
       
 if __name__ == '__main__':
   main()
