771ed533e920de1e0c6e310c77f32d54fa879ede
Sam Murphy <samsammurphy@gmail.com>
2017-01-26 15:27:20 -1000

    added continuous trend line

--- plot_test.py
+++ plot_test.py
@@ -6,6 +6,7 @@ plot_test.py
 
 import pickle
 import datetime
+import numpy
 from matplotlib import pylab as plt
 
 
@@ -50,18 +51,64 @@ def define_plot_space():
   fig = plt.figure()
   chart = fig.add_subplot(1,1,1) # a.k.a. 'axes'
   
-  # time period define
+  # x limits (time period)
   start = datetime.datetime(2000,1,1)
   stop  = datetime.datetime(2016,1,1)
   chart.set_xlim(start, stop)
   
-  # reflectance limits
+  # y limits (reflectance)
   chart.set_ylim(0,0.6)
   
   return chart
 
 
 
+def waveband_colour(waveband):
+  """
+  Assign unqiue color to each waveband
+  """  
+  
+  assign_color = {
+                  'blue':'b',
+                  'green':'g',
+                  'red':'r'
+                  #TODO, color for other bands
+#                  'nir':'',
+#                  'swir1':'',
+#                  'swir2':'',
+                  }
+                  
+  return assign_color[waveband]
+
+  
+  
+def plot_time_series_lines(chart,data):
+  """
+  Plots all time series together using single trend line
+  """
+  
+  for waveband in ['blue','green','red']:
+    
+    dates = []
+    values = []
+    
+    for sat in ['L4','L5','L7','L8','AST']:
+      
+      if data[sat][waveband][0]:
+        dates += data[sat][waveband][0]
+        values += data[sat][waveband][1]
+         
+    #chronologic sort
+    chronological = sorted(zip(dates,values), key=lambda x: x[0])
+    dates = [x[0] for x in chronological]
+    values = [x[1] for x in chronological]
+    
+    colour = waveband_colour(waveband)
+
+    chart.plot(dates,values,colour)  
+  
+
+  
 def plot_satellite_points(chart,data):
   """
   Overplots satellite data points
@@ -75,12 +122,6 @@ def plot_satellite_points(chart,data):
                       'AST':'^',
                       }
   
-  variable_colour = {
-                  'blue':'b',
-                  'green':'g',
-                  'red':'r'
-                  }
-  
   for sat in data.keys():
     symbol = satellite_symbol[sat]
     
@@ -88,10 +129,10 @@ def plot_satellite_points(chart,data):
       
       date = data[sat][waveband][0]
       value = data[sat][waveband][1]
-      colour = variable_colour[waveband] 
+      colour = waveband_colour(waveband)
       
       chart.plot(date,value,colour+symbol)
-    
+   
 
 
 def main():
@@ -105,11 +146,11 @@ def main():
   chart = define_plot_space()
     
   # plot trend line (all satellites)
-  # plot_trend_line(chart,data)
+  plot_time_series_lines(chart,data)
   
   # plot unique symbols for each satellite
   plot_satellite_points(chart,data)
-  
+   
 
 if __name__ == '__main__':
   main()  
