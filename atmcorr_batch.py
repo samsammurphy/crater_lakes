26dc0d23ab006c1eed80819584a1864cb3400b10
Sam Murphy <samsammurphy@gmail.com>
2017-02-05 16:59:34 -1000

    tidy up and added TODO

--- atmcorr_batch.py
+++ atmcorr_batch.py
@@ -10,6 +10,9 @@ Created on Sat Feb  4 12:24:19 2017
 
 from atmcorr_lake_time_series import run_atmcorr
 
+#TODO run through all volcano names, look for satellite files in lake data, if atmcorr for that satellite doesnt already exist then run_atmcorr for that satellite onlu
+
+
 targets = ['Poas','Kusatsu-Shirane','Copahue','Aso']
 
 for target in targets:
