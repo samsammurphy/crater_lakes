58934edc4f657476e27dda3b4185e51ba31301f3
Sam Murphy <samsammurphy@gmail.com>
2017-02-07 11:13:21 -1000

    single exporter

--- export_hack.py
+++ export_hack.py
@@ -15,12 +15,17 @@ from export_LANDSAT_time_series import LANDSAT_export
 
 # volcano names
 #fname = '/home/sam/Dropbox/HIGP/Crater_Lakes/z/Volcanoes/volcano_names.txt'
-fname = '/home/sam/git/crater_lakes/volcanoes_to_get.txt'
-vfile = open(fname, "r")
-for i, line in enumerate(vfile):
-  volcano_name = line.strip('\n')
-  if i <= 10:
-    print(volcano_name)
-    ASTER_export(volcano_name)
-    #time.sleep(120)
-  #LANDSAT_export(volcano_name)
+#fname = '/home/sam/git/crater_lakes/volcanoes_to_get.txt'
+#vfile = open(fname, "r")
+#for i, line in enumerate(vfile):
+#  volcano_name = line.strip('\n')
+#  if i <= 10:
+#    print(volcano_name)
+#    ASTER_export(volcano_name)
+#    #time.sleep(120)
+#    LANDSAT_export(volcano_name)
+
+
+volcano_name = 'Paektu'
+ASTER_export(volcano_name)
+LANDSAT_export(volcano_name)
