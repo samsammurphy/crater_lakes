1b4c2bc3b9aaf927a73439dd6b8fe14337431f00
Sam Murphy <samsammurphy@gmail.com>
2017-02-09 13:54:03 -1000

    simple export hack, updated plot routine

--- export_hack.py
+++ export_hack.py
@@ -9,23 +9,10 @@ Created on Sat Feb  4 09:31:39 2017
 @author: sam
 """
 
-import time
 from export_ASTER_time_series import ASTER_export
 from export_LANDSAT_time_series import LANDSAT_export
 
-# volcano names
-#fname = '/home/sam/Dropbox/HIGP/Crater_Lakes/z/Volcanoes/volcano_names.txt'
-#fname = '/home/sam/git/crater_lakes/volcanoes_to_get.txt'
-#vfile = open(fname, "r")
-#for i, line in enumerate(vfile):
-#  volcano_name = line.strip('\n')
-#  if i <= 10:
-#    print(volcano_name)
-#    ASTER_export(volcano_name)
-#    #time.sleep(120)
-#    LANDSAT_export(volcano_name)
 
-
-volcano_name = 'Yugama'
+volcano_name = 'Kelimutu_c'
 ASTER_export(volcano_name)
 LANDSAT_export(volcano_name)
