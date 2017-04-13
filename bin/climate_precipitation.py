408cb53b82d4bd584978db78f61ea3c77d56bd41
Sam Murphy <samsammurphy@gmail.com>
2017-04-12 15:42:45 -1000

    updated to include more dates

--- bin/climate_precipitation.py
+++ bin/climate_precipitation.py
@@ -18,13 +18,33 @@ df = pd.read_csv('/home/sam/Dropbox/HIGP/Crater_Lakes/Dmitri_Sam/Kelimutu/climat
 df = df.set_index(pd.DatetimeIndex(df['system:index']))
 
 # time slice
-start = df.index.searchsorted(pd.datetime(2012,7,1))
-stop = df.index.searchsorted(pd.datetime(2016,7,1))
+start = df.index.searchsorted(pd.datetime(2013,7,1))
+stop = df.index.searchsorted(pd.datetime(2015,7,1))
 df = df.ix[start:stop]
 
 monthly = df.resample('1M').agg('sum')
 
-plt.plot(monthly.precipitation,'*-')
+fig = plt.figure(figsize=(24,12))
+plt.plot(monthly.precipitation,'--')
+plt.plot(monthly.precipitation,'r*')
 plt.xlabel('Date')
 plt.ylabel('Precipitation (mm)')
+
+
+plot_date_lines = False
+
+if plot_date_lines:
+    plt.axvline(pd.datetime(2013,10,6),color='magenta')
+    plt.axvline(pd.datetime(2013,11,7),color='magenta')
+    plt.axvline(pd.datetime(2014,1,26),color='magenta')
+    plt.axvline(pd.datetime(2014,3,23),color='magenta')
+    plt.axvline(pd.datetime(2014,3,31),color='magenta')
+    
+    plt.axvline(pd.datetime(2014,6,19),color='magenta')
+    plt.axvline(pd.datetime(2014,7,21),color='magenta')
+    plt.axvline(pd.datetime(2014,10,9),color='magenta')
+    plt.axvline(pd.datetime(2014,11,10),color='magenta')
+    plt.axvline(pd.datetime(2015,1,29),color='magenta')
+    plt.axvline(pd.datetime(2015,3,10),color='magenta')
+
 plt.show()
