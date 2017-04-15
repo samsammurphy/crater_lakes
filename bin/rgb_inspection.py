46134b7c18a9ce8b9ef604bc9ff41a7a567ed3b7
Sam Murphy <samsammurphy@gmail.com>
2017-04-15 09:15:38 -1000

    minor changes

--- bin/rgb_inspection.py
+++ bin/rgb_inspection.py
@@ -11,9 +11,9 @@ def rgb_stretch(R,G,B, top):
   return (R,G,B)
 
 
-b = 0.0341458205	
-g = 0.0334175792	
-r = 0.0366226966
+# b = 0.0341458205	
+# g = 0.0334175792	
+# r = 0.0366226966
 
 # b = 0.0268601052	
 # g = 0.0269842867	
@@ -23,12 +23,18 @@ r = 0.0366226966
 # g = 0.0434961468	
 # r = 0.0473292466
 
+# 2013-01-15
+b = 0.0700247203	
+g = 0.0763519408
+r =	0.087073169
+
+
 
 rgb = (r,g,b)
-top = 0.05
+top = 0.15
 
 stretched_rgb = rgb_stretch(rgb[0],rgb[1],rgb[2],top)
 
 print('original rgb = ',rgb)
-print('strecthed rgb = ',stretched_rgb)
+print('strecthed rgb = {0[0]:.6f}, {0[1]:.6f}, {0[2]:.6f}'.format(stretched_rgb))
 
